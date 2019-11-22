9eaadaf45f899c7aea18d8b7d72b9c7a16ce8973
MinaCarnero <orca.romina@gmail.com>
2019-11-21 20:23:10 -0600

    Added some comments in the first part (Project and repository)
    And  modified the comment in the Version control section

--- R Final Project.R
+++ R Final Project.R
@@ -4,11 +4,14 @@
 
 # Project + Github repository ---------------------------------------------
 
+# Name of the Github repository: Final Project
+# Name of the Project: Final-Project
+# Name of the script: R Final Project
 
 # Version Control ---------------------------------------------------------
 
 # First comment " R project"
-# I created an object (a) and then I deleted it (red)
+# I created an object (a) which was a sequence from 1 to 100, and then I deleted it (red)
 
 a <- c(1:100)
 rm(a)
