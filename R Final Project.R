8b129e72393d5955a093d7d89c06a8f6e7c14407
MinaCarnero <orca.romina@gmail.com>
2019-11-21 20:33:45 -0600

    Comments about the evidence of version control that I am providing in my script

--- R Final Project.R
+++ R Final Project.R
@@ -10,8 +10,13 @@
 
 # Version Control ---------------------------------------------------------
 
-# First comment " R project"
-# I created an object (a) which was a sequence from 1 to 100, and then I deleted it (red)
+# Some evidence of version control:
+# I created an object (a) which was a sequence from 1 to 100, and then I deleted it. I made other 
+# modifications in the text as well. All these changes are going to be highlighted in red if you 
+# delete something and in green if you add something (it should be the latest version of your work).
+# This is visible when you hit commit after saving the script. Then, after making a comment and 
+# hitting commit, you hit push and you will be able to see the new version of your script in 
+# your file saved in your Github repository/Final Project/R Final Project
 
 a <- c(1:100)
 rm(a)
