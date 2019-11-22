e81bf169a23f1e94b16f35eb30b1d7c4b9ad83c6
MinaCarnero <orca.romina@gmail.com>
2019-11-21 20:14:59 -0600

    R Final Project - Evidence of version control: Created object a

---
+++ R Final Project.R
@@ -0,0 +1,19 @@
+# FINAL R PROJECT
+# Romina Carnero Huaman
+
+
+# Project + Github repository ---------------------------------------------
+
+
+# Version Control ---------------------------------------------------------
+
+# First comment " R project"
+# I created an object (a) and then I deleted it (red)
+
+a <- c(1:100)
+a
+rm(a)
+
+# Reading-in data ---------------------------------------------------------
+
+
